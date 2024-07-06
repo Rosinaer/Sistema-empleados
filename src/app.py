7154ff788b6c9135d27ce5382c2db80114987b66
Rosinaer <rosinaer@hotmail.com>
2024-07-06 01:21:41 -0300

    Creo instancia de flask

--- src/app.py
+++ src/app.py
@@ -1,1 +1,12 @@
+from flask import Flask
+from flask import render_template
+from flask_mysqldb import MySQL
 
+app = Flask(__name__)
+mysql = MySQL()
+
+
+if __name__ == '__main__':
+     print("Starting Flask app...")
+     app.run(debug=True) 
+ 
