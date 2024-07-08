99bc848289ff4ef190d6a39e14ac04ecc574cedc
Rosina Renones <115019465+Rosinaer@users.noreply.github.com>
2024-07-07 23:28:53 -0300

    modificacion app.py

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 # Configuración de la base de datos
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Ag140209'
+app.config['MYSQL_PASSWORD'] = '********'
 app.config['MYSQL_DB'] = 'proyecto_cac'
 mysql = MySQL(app)
 
