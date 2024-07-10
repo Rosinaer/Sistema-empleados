72588c76f60b30cc4d187a8a125440e537415074
Rosinaer <rosinaer@hotmail.com>
2024-07-09 21:04:15 -0300

    Cree rutas delrte y modyfy

--- src/app.py
+++ src/app.py
@@ -11,6 +11,10 @@ app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = '*******'
 app.config['MYSQL_DB'] = 'proyecto_cac'
+
+UPLOADS = os.path.join('uploads')
+app.config['UPLOADS'] = UPLOADS
+
 mysql = MySQL(app)
 
 
@@ -55,8 +59,64 @@ def store():
     conn.commit()
     
     return redirect('/')
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    sql = "DELETE FROM empleados WHERE id=%s" 
+    conn = mysql.connection 
+    cursor = conn.cursor()
+    cursor.execute(sql, (id, ))  
+    conn.commit()
+
+    return redirect('/')
    
+@app.route('/modify/<int:id>')
+def modify(id):
+    sql = "SELECT * FROM empleados WHERE id=%s" 
+    conn = mysql.connection 
+    cursor = conn.cursor()
+    cursor.execute(sql, (id, ))
+    empleado = cursor.fetchone() 
+    conn.commit()
     
+    return render_template('empleados/edit.html', empleado=empleado)
+
+@app.route('/update', methods=['POST'])
+def updates():
+    _nombre = request.form['txtNombre']
+    _doc = request.form['txtDoc']
+    _dirección = request.form['txtDirección']
+    _correo = request.form['txtCorreo']
+    _foto = request.form['txtFoto']
+    id = request.form['txtId']
+    
+    datos = (_nombre, _doc, _dirección, _correo, id)
+    
+    conn = mysql.connection 
+    cursor = conn.cursor()
+    
+    if _foto.filename != '':
+        now = datetime.now()
+        tiempo = now.strftime("%y%H%M%S")
+        nuevoNombreFoto = tiempo + '_' +_foto.filename
+        _foto.save(os.path.join(app.root_path, 'uploads', nuevoNombreFoto))
+ 
+    sql = 'SELECT foto FROM empleados WHERE id=%s' 
+    cursor.execute(sql)
+    
+    nombreFoto = cursor.fetchone()[0]
+    
+    os.remove(os.path.join(app.config[UPLOADS], nombreFoto))
+    
+    conn = mysql.connection 
+    cursor = conn.cursor()
+    
+    sql = 'UPDATE empleados SET nombre=(_nombre), Doc=(_Doc), dirección=(_dirección), correo=(_correo) WHERE id={id}'
+    
+    conn.commit()
+   
 if __name__ == '__main__':
     print("Starting Flask app...")
     app.run(debug=True)
+
+    
